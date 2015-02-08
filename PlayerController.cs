d96da2e1d6887bee45be777c6541f3727e650012
SteveShaw <steve.shao@yahoo.com>
2015-02-08 12:29:44 +0800

    Add PlayerController Script

---
+++ PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public enum MOVETYPE {IDLE = 0, WALK=1, BOXPUSH=2, CHEER=3};
+
+	public MOVETYPE PlayerState
+	{
+		get{ return State;}
+
+		set
+		{
+			State = value;
+
+			AnimComp.SetInteger("iState",(int)State);
+		}
+	}
+
+	private MOVETYPE State = MOVETYPE.IDLE;
+
+	public float MoveTime = 1.0f;
+
+	public float MoveDistance = 2.0f;
+
+	private Transform ThisTransform = null;
+
+	private Animator AnimComp = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
