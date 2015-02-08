6fb5cdc186d880911240d76ea0b47384833e5635
SteveShaw <steve.shao@yahoo.com>
2015-02-08 12:57:22 +0800

    Update control

--- PlayerController.cs
+++ PlayerController.cs
@@ -28,11 +28,64 @@ public class PlayerController : MonoBehaviour {
 	private Animator AnimComp = null;
 	// Use this for initialization
 	void Start () {
+
+		ThisTransform = GetComponent<Transform> ();
+		
+		AnimComp = GetComponent<Animator>();
+		
+		StartCoroutine(HandleInput());
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public IEnumerator HandleInput()
+	{
+		while(true)
+		{
+			while(Mathf.CeilToInt(Input.GetAxis("Vertical"))>0)
+			{
+				PlayerState = MOVETYPE.WALK;
+				
+				yield return StartCoroutine(Move(MoveDistance));
+			}
+			
+			PlayerState = MOVETYPE.IDLE;
+			
+			yield return null;
+		}
 	}
+	
+	public IEnumerator Move(float Increment=0)
+	{
+		//Start Position
+		Vector3 StartPos = ThisTransform.position;
+		
+		//Dest Position
+		
+		Vector3 DestPos = ThisTransform.position + ThisTransform.forward * Increment;
+		
+		//Elapsed Time
+		
+		float ElapsedTime = 0.0f;
+		
+		while(ElapsedTime < MoveTime)
+		{
+			//Calculate interpolated angle
+			Vector3 FinalPos = Vector3.Lerp(StartPos,DestPos,ElapsedTime/MoveTime);
+			
+			//update pos
+			ThisTransform.position = FinalPos;
+			
+			//Wait until next frame
+			yield return null;
+			
+			ElapsedTime += Time.deltaTime;
+		}
+		
+		//Complete Move
+		ThisTransform.position = DestPos;
+		
+		yield break;
+		
+	}
+	
 }
